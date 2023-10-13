f73af88a3d615008b5da3805b13fb9110fbd835f
Dimonti <d1mont1@mail.ru>
2023-10-13 21:40:10 +0600

    Добавьте файлы проекта.

---
+++ dotNET-module-5-practice/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNET_module_5_practice
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                // Вызываем метод, который может сгенерировать исключение
+                CallMethodWithException();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Произошла ошибка: " + ex.Message);
+            }
+        }
+
+        // Метод, который генерирует исключение
+        static void MethodThatThrowsException()
+        {
+            throw new InvalidOperationException("Исключение внутри MethodThatThrowsException");
+        }
+
+        // Метод, который вызывает MethodThatThrowsException и "поднимает" исключение
+        static void CallMethodWithException()
+        {
+            try
+            {
+                MethodThatThrowsException();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Произошла ошибка в CallMethodWithException: " + ex.Message);
+                // Поднимаем исключение в вызывающий метод
+                throw;
+            }
+        }
+    }
+
+}
